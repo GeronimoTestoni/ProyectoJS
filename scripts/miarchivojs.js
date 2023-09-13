3004cec832d0f2fbe03b37cefe6539e89cb3367d
Geronimo Testoni <testonigeronimo@hotmail.com>
2023-09-13 19:04:58 -0300

    scripter

--- scripts/miarchivojs.js
+++ scripts/miarchivojs.js
@@ -1,18 +1,21 @@
 
 var boton = document.getElementById("miBoton");
-    boton.addEventListener("click", function() {
-        alert("Es inevitable tocar un boton cuando lo vemos ¿ no ? ");
-    });
+boton.addEventListener("click", function () {
+    alert("Es inevitable tocar un boton cuando lo vemos ¿ no ? ");
+});
 
 console.log("Hola Profe Hola tutores, bienvenido!");
 
-    // Ejemplo para calcular nota final de alumno //
+// Ejemplo para calcular nota final de alumno //
 
-    var nota1 = parseFloat(prompt("Ingrese la nota de la primera evaluación:"));
-    var nota2 = parseFloat(prompt("Ingrese la nota de la segunda evaluación:"));
-    var nota3 = parseFloat(prompt("Ingrese la nota de la tercera evaluación:"));
+alert("Bienvenido a la calculadora para avergiuar su nota final. Por favor, ingrese el resultado de sus pruebas.");
 
-    var notaFinal = (nota1 + nota2 + nota3) / 3;
 
-    console.log("La nota final del alumno es: " + notaFinal.toFixed(2));
+var nota1 = parseFloat(prompt("Ingrese la nota de la primera evaluación:"));
+var nota2 = parseFloat(prompt("Ingrese la nota de la segunda evaluación:"));
+var nota3 = parseFloat(prompt("Ingrese la nota de la tercera evaluación:"));
+
+var notaFinal = (nota1 + nota2 + nota3) / 3;
+
+console.log("La nota final del alumno es: " + notaFinal.toFixed(2));
 
